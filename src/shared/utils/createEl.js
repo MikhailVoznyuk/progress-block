52ea7102edf002d77d68095df78a147685d8aaba
MikhailVoznyuk <mikhailv.commerce@gmail.com>
2026-02-22 16:16:00 +0300

    update: createEl.js

--- src/shared/utils/createEl.js
+++ src/shared/utils/createEl.js
@@ -1,33 +1,43 @@
 export function createEl(tag, props, ...children) {
     const el = document.createElement(tag);
+    if (props) {
+        for (const [key, value] of Object.entries(props)) {
+            if (value === null || value === undefined || value === false) continue;
 
-    for (const [key, value] of Object.entries(props)) {
-        if (!value) continue;
-
-        if (key === 'class' && typeof(value) === 'string') {
-            el.className = value;
-        } else if (key === 'style' && typeof(value) === 'object') {
-            Object.assign(el.style, value)
-        } else if (key === 'data' && typeof(value) === 'object') {
-            for (const [dataKey, dataValue] of Object.entries(value)) {
-                el.dataset[dataKey] = String(dataValue);
+            if (key === 'className' && typeof(value) === 'string') {
+                el.className = value;
+            } else if (key === 'style' && typeof(value) === 'object') {
+                Object.assign(el.style, value)
+            } else if (key === 'data' && typeof(value) === 'object') {
+                for (const [dataKey, dataValue] of Object.entries(value)) {
+                    el.dataset[dataKey] = String(dataValue);
+                }
+            } else if (key.startsWith('on') && typeof(value) === 'function') {
+                el.addEventListener(key.slice(2).toLowerCase(), value);
+            } else {
+                if (key in el) {
+                    el[key] = value;
+                } else {
+                    el.setAttribute(key, String(value))
+                }
             }
-        } 
-        else if (key.startWith('on') && typeof(value) === 'function') {
-            el.addEventListener(key.slice(2), value);
-        }
+    }
     }
 
     const addChild = (child) => {
-        if (!child) return;
+        if (child === null || child === undefined || child === false) return;
 
         if (Array.isArray(child)) {
-            item.forEach(addChild);
-        } else if (child.instanceOf(Node)) {
+            child.forEach(addChild);
+        } else if (child instanceof Node) {
             el.appendChild(child);
-        }
+        } else if (typeof(child) === 'string' || typeof(child) === 'number') {
+            el.appendChild(document.createTextNode(String(child)));
+        } else {
+             throw new Error('Unexpected type of child')
+        } 
     }
 
-    addChild(children)
+    children.forEach(addChild);
     return el;
 }
